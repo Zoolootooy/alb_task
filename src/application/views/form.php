762f35b1a06352a71993e3d4a7a3341604fa14d9
Your Name <maxradjia.zp@gmail.com>
2020-07-21 13:27:09 +0300

    Added map on view form

---
+++ src/application/views/form.php
@@ -0,0 +1,8 @@
+<?php require('partials/header.php'); ?>
+
+<div id="map"></div>
+<script src="../../public/js/map.show.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAk10RdU1wJJ7UZnIZj8XBuVQopBvicRPE&callback=initMap"
+        async defer></script>
+
+<?php require('partials/footer.php'); ?>
